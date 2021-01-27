30f212910c598270105382c90ef24ec3faa45d91
bojan <gagaleskibojan@gmail.com>
2021-01-27 11:09:52 +0100

    newDesign

--- script.js
+++ script.js
@@ -17,17 +17,26 @@ function todoNewElement(){
     todoInput.value = ''
     setElements()
     }else{
-        alert("ITS EMPTY")
+        alert("You cannot enter empty task")
     }
 }
 //LOOP THROUGH ELEMENTS IN THE ARR and write them as LI elements(WORKS)
 function setElements(){
-    if(arr.length !== 0){
+    if(arr.length){
         arr.forEach(() => {
             todoList.innerHTML = "";
             for (var i = 0; i < arr.length; i++)
-            todoList.innerHTML +=`<li id='${i}' class="liE" ><span id="a${i}">${arr[i]}</span><span class="remove"  onclick = "removeEl(${i})"><img src="/trash.png" alt="">
-            </span><span class="edit"  onclick = "editEl(${i})"><img src="/edit.png" alt=""></span><span class="save" onclick = "saveEl(${i})"><img src="/save.png" alt=""></span></li>`
+            todoList.innerHTML +=`<li id="${i}" class="liE">
+            <span id="a${i}">${arr[i]}</span>
+            <div class="func">
+            <span class="remove" onclick="removeEl(${i})"
+              ><img src="/trash.png" alt="" /> </span
+            ><span class="edit" onclick="editEl(${i})"
+              ><img src="/edit.png" alt="" /></span
+            ><span class="save" onclick="saveEl(${i})"
+              ><img src="/save.png" alt=""
+            /></span>
+          </div>`
             
     })}else{
       todoList.innerHTML = ""
@@ -54,7 +63,6 @@ function editEl(index){
 }
 function re(i){
     var newList = document.getElementById(i).innerText
-    console.log(newList)
     arr.splice(i, 1, newList);
     if (localStorage.getItem("list") == null) {
       localStorage.setItem("list", JSON.stringify(arr));
@@ -62,6 +70,11 @@ function re(i){
       localStorage.setItem("list", JSON.stringify(arr));
     }
     setElements();
+    document.getElementById("a"+i).addEventListener("keydown", (e)=>{
+      if(e.keyCode == 13){
+        saveEl(i)
+      }
+    })
 }
 function saveEl(i){
   document.getElementById("a"+i).contentEditable = 'false'
@@ -74,6 +87,12 @@ function saveEl(i){
   }
   setElements();
 }
+
 //EVENT LISTENERS
 todoBtn.addEventListener("click",todoNewElement)
+todoInput.addEventListener('keydown' , (e)=>{
+  if(e.keyCode == 13){
+    todoNewElement()
+  }
+})
 setElements()
